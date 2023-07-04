fb31294ca8475c3c99ce69ff86c0a60fc6a33f2e
nja93 <njanjalchege@gmail.com>
2023-07-04 05:57:46 -0700

    Repetition Token #2

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
-# This is a ruby script that matches  hbt with "t" icrementing to t*5 ending with n
+# This is a ruby script that matches  hbt with "t" icrementing to t*4 ending with n
+
 puts ARGV[0].scan(/hbt{1,4}n/).join
