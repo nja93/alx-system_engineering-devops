588261195ee4c7e07b0fdcd70b0a785f4ab6af38
nja93 <njanjalchege@gmail.com>
2023-08-25 09:50:33 -0700

    fixed removed spaces

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { '/tmp/school':
-  ensure  => file,
+file {'/tmp/school':
+  ensure  => filepresent,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => "I love Puppet\n",
+  content => "I love Puppet",
 }
