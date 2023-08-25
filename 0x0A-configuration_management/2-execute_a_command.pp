93ada5f99536ed86ae457f5e77adb0638a3538cc
nja93 <njanjalchege@gmail.com>
2023-08-25 10:47:09 -0700

    create a manifest that kills a process named killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# A manifest that kills a process named killmenow
+# Must use the exec Puppet resource
+# Must use pkill
+exec {'killmenow':
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin/'
+}
